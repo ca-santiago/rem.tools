92f33246ece446e544b010adf0783fcf00eb99f4
Santiago <greminoficial@gmail.com>
2021-05-07 21:25:53 -0500

    Cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({ methods: '*', origin: '*' });
+  app.enableCors();
   await app.listen(process.env.PORT || 3300);
 }
 bootstrap();
